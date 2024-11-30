81478b66956c0b97dfe2c6e1ab09f4ce754a75e1
voyagerdva <mariner-9@yandex.ru>
2024-11-30 18:08:21 +0300

    saved h5

--- app.py
+++ app.py
@@ -16,57 +16,57 @@ Dense = layers.Dense
 to_categorical = utils.to_categorical
 load_model = models.load_model
 
-
-
-
-# Проверим, можно ли загрузить данные
-(x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
-
-# Преобразование размерности изображений и нормализация данных:
-x_train = x_train.reshape(60000, 784).astype("float32") / 255
-
-# Преобразуем метки в категории:
-y_train = utils.to_categorical(y_train, 10)
-
-# Названия классов:
-classes = ["футболка", "брюки", "свитер", "платье", "пальто", "туфли", "рубашка", "кроссовки", "сумка", "ботинки"]
-
-# Создаем последовательную модель:
-model = Sequential()
-
-# Добавляем уровни сети:
-model.add(Dense(800, input_dim=784, activation='relu'))
-model.add(Dense(10, activation="softmax"))
-
-# компилируем модель:
-model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
-
-print(model.summary())
-
-# Обучаем сеть:
-model.fit(
-    x_train,
-    y_train,
-    batch_size=200,
-    epochs=100,
-    validation_split=0.2,
-    verbose=1
-)
-
-model.save('fashion_mnist_dense.h5')
-model = load_model('fashion_mnist_dense.h5')
-model.save_weight('fashion_mnist_weights.h5')
-model.load_weights('fashion_mnist_weights.h5')
+#
+#
+#
+# # Проверим, можно ли загрузить данные
+# (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+#
+# # Преобразование размерности изображений и нормализация данных:
+# x_train = x_train.reshape(60000, 784).astype("float32") / 255
+#
+# # Преобразуем метки в категории:
+# y_train = utils.to_categorical(y_train, 10)
+#
+# # Названия классов:
+# classes = ["футболка", "брюки", "свитер", "платье", "пальто", "туфли", "рубашка", "кроссовки", "сумка", "ботинки"]
+#
+# # Создаем последовательную модель:
+# model = Sequential()
+#
+# # Добавляем уровни сети:
+# model.add(Dense(800, input_dim=784, activation='relu'))
+# model.add(Dense(10, activation="softmax"))
+#
+# # компилируем модель:
+# model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+#
+# print(model.summary())
+#
+# # Обучаем сеть:
+# model.fit(
+#     x_train,
+#     y_train,
+#     batch_size=200,
+#     epochs=100,
+#     validation_split=0.2,
+#     verbose=1
+# )
+#
+# model.save('./fashion_mnist_dense.h5')
+model = load_model('/.fashion_mnist_dense.h5')
+model.save_weights('/.fashion_mnist.weights.h5')
+model.load_weights('/.fashion_mnist.weights.h5')
 
 json_string = model.to_json()
 model = model_from_json(json_string)
 
-# Запускаем сеть на входных данных
-predictions = model.predict(x_test)
-
-# Выводим один из результатов распознавания
-print(predictions[0])
-
-# Выводим номер класса, предсказанный нейросетью
-print(np.argmax(y_train[0]))
+# # Запускаем сеть на входных данных
+# predictions = model.predict(x_test)
+#
+# # Выводим один из результатов распознавания
+# print(predictions[0])
+#
+# # Выводим номер класса, предсказанный нейросетью
+# print(np.argmax(y_train[0]))
 
